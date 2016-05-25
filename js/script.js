fc07190c2b7874d46e9b90ab778d31a801c527a0
oilylyme <elynorfreyss@gmail.com>
2016-05-25 17:51:40 -0400

    refactored text add

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('document').ready(function() {
+$(function() {
   let num;
   do {num = parseInt(prompt("How high should FizzBuzz count?", "100 is the standard."));} 
     while (isNaN(num) || num < 1);
@@ -12,13 +12,17 @@ function fBuzz(start, end) {
   
   for (let i = start;i != end + inc;i += inc) {
     if (i % 3 == 0 && i % 5 == 0) {
-      $('body').append("<p>FizzBuzz</p>");
+      addPara('FizzBuzz');
     } else if (i % 3 == 0) {
-      $('body').append("<p>Fizz</p>");
+      addPara('Fizz');
     } else if (i % 5 == 0) {
-      $('body').append("<p>Buzz</p>");
+      addPara('Buzz');
     } else {
-      $('body').append("<p>" + i + "</p>");
+      addPara(i);
     };
   };
 };
+
+function addPara(txt) {
+  $('body').append("<p>" + txt + "</p>");
+}
